71fc352a4192eb88745717f7e8cb781536917b2d
battorydev <113938540+battorydev@users.noreply.github.com>
2023-04-13 15:49:24 +0700

    initial react

---
+++ src/components/AddPost.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AddPost() {
+  return (
+    <div>
+        <input type="text" placeholder="Title" />
+    </div>
+  )
+}
+
+export default AddPost
---
+++ src/components/PostSection.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AddPost from './AddPost'
+import PostList from './PostList'
+
+function PostSection() {
+  return (
+    <div>
+        <AddPost />
+        <PostList />
+    </div>
+  )
+}
+
+export default PostSection
